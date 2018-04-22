607f696ca1f2ca63b8ffc5f04710d25bd9f8f081
Pencil <9807175@qq.com>
2018-04-22 16:26:16 +0800

    updated: improved signal segmentation code

--- signal/seg_var.m
+++ signal/seg_var.m
@@ -1,137 +1,67 @@
-function [ result,peakBeg,peakEnd ] = seg_var( data,fs )
-%使用方差表示信号的能量
+function [ peakBeg, peakEnd ] = seg_var( data, fs )
+% 切段前请先滤掉直流分量
 
-    % 参数设置
-    dataLen=length(data);   % 信号长度
-    frameEnergy=[];         % 帧能量
-    frameLen=200/1000*fs;   % 帧长
-    frameMove=20/1000*fs;   % 帧移
-    frameNum=(dataLen-frameLen)/frameMove+1;    % 帧数
-    thres=0.1;             % 阈值
-    minPeakLen=4;           % 可接受的一个峰的最短总长度（帧），如果小于该长度，则将该峰删除
-    minInterval=5;          % 可接受的相邻峰最小间隔（帧），如果相邻两峰小于该间隔，则后一个峰被删除
-    peakLen=8;             % 一个峰需要的长度（帧）
-    delBegLen=40;           % 信号开头需要平滑的峰长度，如不需要平滑信号开头可设为0
-    Debug=1;                % 是否画图
-    isWarning=1;            % 是否警告
-    
-    % 获取帧能量
-    for i=1:frameNum
-        frameBeg=(i-1)*frameMove+1;             % 每帧的开始位置
-        frameEnd=frameBeg+frameLen-1;           % 每帧的结束位置
-        tmpFrame=data(frameBeg:frameEnd);       % 获取当前帧
-        tmpFrame=(tmpFrame-mean(tmpFrame));
-        tmpFrame=tmpFrame.*hamming(frameLen)';  % 加窗（汉明窗）
-        tmpEnergty=sum(tmpFrame.^2);            % 计算方差
-        frameEnergy=[frameEnergy,tmpEnergty];   % 记录帧能量
-    end
-    
-    % 把前面几帧变小，使前面几帧不影响阈值判断
-    for i=1:delBegLen
-        frameEnergy(i)=frameEnergy(i)*i/delBegLen/2;
-    end
-    
-    % 寻找起始点与结束点
-    begFrame=[];
-    endFrame=[];
-    flag=1;
-    for i=1:frameNum
-        % 起始点
-        if frameEnergy(i)>thres && flag==1
-            begFrame=[begFrame,i];
-            flag=0;
-        end
-        % 结束点
-        if frameEnergy(i)<=thres && flag==0
-            endFrame=[endFrame,i];
-            flag=1;
-        end
-    end
-    
-    % 删除极短峰
-    peakNum=length(endFrame);
-    i=1;
-    while i<=peakNum
-        if endFrame(i)-begFrame(i)<minPeakLen
-            if isWarning==1
-                disp('发现极短峰！已删除。')
-            end
-            begFrame(i)=[];
-            endFrame(i)=[];
-            i=i-1;
-            peakNum=peakNum-1;
-        end
-        i=i+1;
-    end
-    
-    % 合并相邻峰
-    peakNum=length(endFrame);
-    i=2;
-    while i<=peakNum
-        if begFrame(i)-endFrame(i-1)<minInterval
-            if isWarning==1
-                disp('发现相邻峰相隔太近！已删除后一个峰。')
-            end
-            begFrame(i)=[];
-            endFrame(i)=[];
-            i=i-1;
-            peakNum=peakNum-1;
-        end
-        i=i+1;
-    end
-    
-    % 异常检测
-    if isempty(begFrame)
-        disp('没有切割到峰！');
-        result=-1;
-        return
-    end
-    
-    % 获取原始数据的起始与结束位置
-    endFrame=begFrame+peakLen;      %输出峰取固定长度
-    begFrame=begFrame-3;            %增加缓存区
-    endFrame=endFrame+10;           %增加缓存区
-    peakBeg=begFrame.*frameMove;    %计算真实起始位置
-    peakEnd=endFrame.*frameMove;    %计算真实结束位置
-    result=1;
-    
-    % 出现结束点超过信号长度的情况
-    if peakEnd(end)>dataLen
-        if peakEnd(end)-dataLen>peakLen*frameMove*0.25
-            % 该峰不够完整
-            disp('起始数组与结束数组不一致！已自动删除最后一个起始点');
-            peakBeg(end)=[];
-            peakEnd(end)=[];
+    peakBeg = [];
+    peakEnd = [];
+
+    % Config
+    len = length(data);     % 信号总长度
+    signalLen = 0.3 * fs;   % 单个信号长度
+    DEBUG = true;           % 是否画图
+    THRESHOLD = 0.006;
+    MIN_LEN = floor(signalLen * 0.3);
+
+    % 将信号数据转换为比较好识别信号的一个数组
+    % 设某一帧的数据为 data
+    % 则每一帧的值计算方法为 x = abs(mean(diff(data))) / max(data);
+    absData = abs(data);
+    values = zeros(1, len);
+    lessCount = 0;
+    highlightX = [];
+    highlightY = [];
+    lastDiffValue = sum(diff(absData(1:signalLen)));
+    for i=signalLen + 1 : len
+        frameData = absData(i - signalLen:i);
+        currentDiffValue = lastDiffValue - (absData(i - signalLen + 1) - absData(i - signalLen)) + (absData(i) - absData(i - 1));
+        values(i) = abs(currentDiffValue / signalLen) / max(frameData);
+        lastDiffValue = currentDiffValue;
+        if values(i) < THRESHOLD
+            lessCount = lessCount + 1;
         else
-            % 该峰足够完整
-            disp('起始数组与结束数组不一致！已自动取信号尾部为结束点');
-            peakEnd(end)=dataLen;      %计算真实结束位置
-            peakBeg(end)=peakEnd(end)-(peakEnd(1)-peakBeg(1))+1;    %计算真实起始位置
+            m = i - lessCount;
+            b = m - signalLen;
+            e = m + signalLen;
+            if lessCount >= MIN_LEN && sum(data(b:m) .^ 2) / sum(data(m:e) .^ 2) > 6
+                highlightX = [highlightX, m:i];
+                highlightY = [highlightY, data(m:i)];
+                peakBeg = [peakBeg, b];
+                peakEnd = [peakEnd, e];
+            end
+            lessCount = 0;
         end
     end
-    
+
     % 画图
-    if Debug==1
-        % 原图像
+    if DEBUG
         close all;
-        h=figure(1);
-        set(h,'Position',[10,410,500,350]);
-        plot(data);
+        figure;
+        subplot(2, 1, 1);
         hold on;
+        % 原图像
+        plot(data);
+        % 高亮小于阈值的点
+        for i=1:length(highlightX)
+            plot(highlightX(i), highlightY(i), '.r');
+        end
+        % 切割位置
         for i=1:length(peakBeg)
-            plot([peakBeg(i),peakBeg(i)],[min(data),max(data)],'r');
-            plot([peakEnd(i),peakEnd(i)],[min(data),max(data)],'r');
+            plot([peakBeg(i), peakBeg(i)],[min(data),max(data)],'b');
+            plot([peakEnd(i), peakEnd(i)],[min(data),max(data)],'r');
         end
 
-        % 帧能量图像
-        h=figure(2);
-        set(h,'Position',[510,410,500,350]);
-        plot(frameEnergy);
+        subplot(2, 1, 2);
         hold on;
-        for i=1:length(begFrame)
-            plot([begFrame(i),begFrame(i)],[0,max(frameEnergy)],'r');
-            plot([endFrame(i),endFrame(i)],[0,max(frameEnergy)],'r');
-        end
+        plot(values);
     end
 end
 
